15726a0d09af998f0a90ee38b0e0fc55fef97929
DrStealth <dr.stealth1@gmail.com>
2016-09-20 13:37:59 -0600

    Added Compressor and Soul Shard (not yet fully implemented). Added crafting recipes for Tank and Compressor.

---
+++ src/main/java/com/drstealth/soulcraft/blocks/BlockSoulCompressor.java
@@ -0,0 +1,8 @@
+package com.drstealth.soulcraft.blocks;
+
+/**
+ * Created by DrStealth on 9/20/2016.
+ */
+public class BlockSoulCompressor
+{
+}
---
+++ src/main/java/com/drstealth/soulcraft/items/ItemSoulShard.java
@@ -0,0 +1,8 @@
+package com.drstealth.soulcraft.items;
+
+/**
+ * Created by DrStealth on 9/20/2016.
+ */
+public class ItemSoulShard
+{
+}
